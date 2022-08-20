d3f133d91210d981a8846ba4e91170d97038d9fc
husinzz <its.husin@gmail.com>
2022-08-21 04:05:45 +0700

    delete : removed react index

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>The template works</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
